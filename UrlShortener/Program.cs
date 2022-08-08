7823bd2e6febfcce8f41ba660e33c9aed68c5996
Dmitry <vylegzhanin.d.a@mail.ru>
2022-08-08 14:19:51 +0500

    Init

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -24,7 +24,6 @@ using (var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>(
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
